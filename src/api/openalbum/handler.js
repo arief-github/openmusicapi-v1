2f2131cca109700b6bc54ea46d03ec4b7c64cca1
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-23 10:37:50 +0700

    Update openAlbumHandler : Passed postman test POST API

--- src/api/openalbum/handler.js
+++ src/api/openalbum/handler.js
@@ -10,20 +10,40 @@ class AlbumsHandler {
     }
 
     async postAlbumHandler(req, h) {
-        const { name = 'untitled', year } = req.payload;
+        try {
+            const { name = 'untitled', year } = req.payload;
 
-        const albumId = await this._service.addAlbum({ name, year });
+            const albumId = await this._service.addAlbum({ name, year });
 
-        const response = h.response({
-            status: 'success',
-            message: 'album berhasil ditambahkan',
-            data: {
-                albumId,
+            const response = h.response({
+                status: 'success',
+                message: 'album berhasil ditambahkan',
+                data: {
+                    albumId,
+                }
+            })
+            response.code(201);
+            return response;
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'error',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
             }
-        })
 
-        response.code(201);
-        return response;
+            // Server ERROR!
+            const response = h.response({
+                status: 'fail',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(400);
+            console.error(error);
+            return response;
+        }
+
     }
 
     async getAlbumHandler() {

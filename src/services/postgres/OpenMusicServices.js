47f71e6356a2d212d32267d57f887eab38548291
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-31 11:49:45 +0700

    Fix typo

--- src/services/postgres/OpenMusicServices.js
+++ src/services/postgres/OpenMusicServices.js
@@ -42,7 +42,7 @@ class OpenMusicsService {
     async getSongById(id) {
         const query = {
             text: 'SELECT * FROM songs WHERE id = $1',
-            valuse: [id],
+            values: [id],
         }
 
         const result = await this._pool.query(query);
@@ -54,10 +54,10 @@ class OpenMusicsService {
         return result.rows.map(openMusicModel)[0];
     }
 
-    async editSongById(id, { title, year, genre, performer, duration, albumId }) {
+    async editSongById(id, { title, year, genre, performer, duration }) {
         const query = {
-            text: "UPDATE songs SET title=$1, year=$2, genre=$3, performer=$4, duration=$5 WHERE id=$6 RETURNING id",
-            values: [title, year, genre, performer, duration, id],
+            text: "UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, updated_at = $6 WHERE id = $7 RETURNING id",
+            values: [title, year, genre, performer, duration, updatedAt, id],
         }
 
         const result = await this._pool.query(query);
@@ -69,7 +69,7 @@ class OpenMusicsService {
 
     async deleteSongById(id) {
         const query = {
-            text: 'DELETE FROM songs WHERE id=$1 RETURNING id',
+            text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
             values: [id],
         }
 
